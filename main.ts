ea8aec94c8a1ae234add0f22cc462bde5c54f326
colehuffman <79929352+colehuffman@users.noreply.github.com>
2021-03-18 00:01:18 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,60 +1,85 @@
-let Coin_1 = 0
 let Heads = 0
 let Tails = 0
+let P1 = 0
+let P2 = 0
+let Coin_1 = 0
 let Coin_2 = 0
-let Player_1 = 0
-let Player_2 = 0
+/**
+ * <---- all coin variables are set to 0 to be ready for next button press
+ */
 input.onButtonPressed(Button.A, function () {
-    // Picks a number between 1 and 2
-    Coin_1 = randint(1, 2)
-    if (Coin_1 == 1) {
-        // If the number is 1 the Heads variable will increase by 1
-        Heads += 1
-        basic.showString("H")
-        basic.pause(200)
-        basic.clearScreen()
-    } else {
-        // If the number picked is not 1 the Tails variable will increase by 1
-        Tails += 1
-        basic.showString("T")
-        basic.pause(200)
-        basic.clearScreen()
+    // loops inside code twice increasing the index variable by 1 each time
+    for (let index = 0; index <= 1; index++) {
+        // if index variable is 0 the microbit runs the "First Coin" function
+        // if index variable is 1 the microbit runs the "Second Coin" function
+        if (index == 0) {
+            First_Coin()
+        } else if (index == 1) {
+            Second_Coin()
+        }
     }
-    Coin_2 = randint(1, 2)
-    if (Coin_2 == 1) {
-        // If the number picked is 1 the Heads variable will increase by 1
-        Heads += 1
-        basic.showString("H")
-        basic.pause(200)
-        basic.clearScreen()
-    } else {
-        // If the number picked is not 1 the Tails variable will increase by 1
-        Tails += 1
-        basic.showString("T")
-        basic.pause(200)
-        basic.clearScreen()
-    }
-    // Awards points depending on heads and tails variables
     if (Heads == 2 || Tails == 2) {
-        Player_1 += 1
+        // If both coins roll the same, P1's score increases by 1
+        P1 += 1
     } else if (Heads == 1 && Tails == 1) {
-        Player_2 += 1
+        // if the coins roll opposite, P2's score increases by 1
+        P2 += 1
     }
-    // Sets variables to 0 to be ready for the next button press
     Coin_1 = 0
     Coin_2 = 0
     Heads = 0
     Tails = 0
 })
-// Resets variables to 0
-input.onButtonPressed(Button.AB, function () {
-    control.reset()
-})
+function Second_Coin () {
+    // sets Coin 2 to 0 or 1
+    Coin_2 = randint(0, 1)
+    // If Coin 2 equals 0 the Heads variable increases by 1 and an H is displayed
+    // If Coin 2 ≠ 0 the Tails variable increases by 1 and a T is displayed
+    if (Coin_2 == 0) {
+        Heads += 1
+        basic.showString("H")
+    } else if (Coin_1 != 0) {
+        Tails += 1
+        basic.showString("T")
+    }
+    // clears screen
+    basic.clearScreen()
+    // pauses for 200 ms
+    basic.pause(200)
+}
 // when button B is pressed the players scores are shown
 input.onButtonPressed(Button.B, function () {
     basic.showString("P1")
-    basic.showString("" + (Player_1))
+    basic.showString("" + (P1))
     basic.showString("P2")
-    basic.showString("" + (Player_2))
+    basic.showString("" + (P2))
     basic.clearScreen()
+})
+function First_Coin () {
+    // Sets Coin 1 to 0 or 1
+    Coin_1 = randint(0, 1)
+    // If Coin 1 equals 0 the Heads variable increases by 1 and an H is displayed
+    // If Coin 1 ≠ 0 the Tails variable increases by 1 and a T is displayed
+    if (Coin_1 == 0) {
+        Heads += 1
+        basic.showString("H")
+    } else if (Coin_1 != 0) {
+        Tails += 1
+        basic.showString("T")
+    }
+    // clears screen
+    basic.clearScreen()
+    // pauses for 200 ms
+    basic.pause(200)
+}
+basic.forever(function () {
+    // If P1 reaches a score of 3 the microbit displays that "P1 Wins" and resets
+    // if P2 reaches a score of 3 the microbit displays "P2 Wins" and resets
+    if (P1 == 3) {
+        basic.showString("P1 Wins")
+        control.reset()
+    } else if (P2 == 3) {
+        basic.showString("P2 Wins")
+        control.reset()
+    }
 })
